168323d965cf39a8ad052f5cb286b2cd9bae4b00
Yuri Parijs <yuriparijs@hotmail.com>
2024-08-28 21:06:38 +0200

    Berichten worden niet meer gerenderd vanuit het ChatHistory object, maar vanuit een eigen collectie van ChatMessages. Hiermee kunnen we aangeven dat er nog gestreamed wordt en hoeven we bij het renderen niet te filteren binnen de ChatHistory. Daarnaast ook een Alert toegevoegd die tijdelijk wordt getoond als er een exception optreed tijdens de communicatie met de assistent.

--- Cv.Aspire.Web/Components/Pages/Code Examples/Assistant.razor.cs
+++ Cv.Aspire.Web/Components/Pages/Code Examples/Assistant.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.Timers;
 
 namespace Cv.Aspire.Web.Components.Pages.Code_Examples;
 
@@ -12,31 +13,82 @@ public partial class Assistant
 
     private string prompt = "";
 
-    private ChatHistory chat = new("When requested, you can change the theme colors of the app using CSS colors.");// Je kan hier de persona mee geven
-    // NIET de ChatHistory direct gebruiken voor renderen -> Wordt gevuld met rare shit tijdens function calling
+    private ChatHistory chat = new("When requested, you can change the theme colors of the app using CSS colors.");
+    private List<ChatMessage> messages = [];
+    private bool showAssistantAlert = false;
+    private System.Timers.Timer timer = new(5000);
+
+    protected override void OnInitialized()
+    {
+        timer.Elapsed += OnAlertFinish;
+        timer.AutoReset = false;
+    }
 
     private async Task Submit()
     {
-        var response = string.Empty;
         try
         {
             chat.AddUserMessage(prompt);
+            messages.AddUserMessage(prompt);
             prompt = string.Empty;
             StateHasChanged();
 
-            chat.AddAssistantMessage(response);
-            var assistantMessage = chat.Last();
+            var response = string.Empty;
+            var assistantMessage = messages.AddStreamingAssistantMessage();
             OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
             var chunks = ChatService.GetStreamingChatMessageContentsAsync(chat, settings, Kernel);
             await foreach (var chunk in chunks)
             {
-                assistantMessage.Content += chunk;
+                response += chunk;
+                assistantMessage.Message += chunk;
                 StateHasChanged();
             }
+
+            assistantMessage.IsStreaming = false;
+            chat.AddAssistantMessage(response);
+            StateHasChanged();
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            throw;
+            HandleSubmitException();
         }
+    }
+
+    private void HandleSubmitException()
+    {
+        timer.Stop();
+        timer.Start();
+        showAssistantAlert = true;
+        messages.RemoveAt(messages.Count - 1);
+        StateHasChanged();
+    }
+
+    private void OnAlertFinish(object? source, ElapsedEventArgs e)
+    {
+        InvokeAsync(() =>
+        {
+            showAssistantAlert = false;
+            StateHasChanged();
+        });
+    }
+}
+
+public record ChatMessage
+{
+    public string Message { get; set; } = string.Empty;
+    public AuthorRole Role { get; set; }
+    public bool IsStreaming { get; set; }
+}
+
+public static class ChatMessageExtensions
+{
+    public static void AddUserMessage(this List<ChatMessage> messages, string message) =>
+        messages.Add(new ChatMessage { Message = message, Role = AuthorRole.User, IsStreaming = false });
+
+    public static ChatMessage AddStreamingAssistantMessage(this List<ChatMessage> messages)
+    {
+        var message = new ChatMessage { Message = string.Empty, Role = AuthorRole.Assistant, IsStreaming = true };
+        messages.Add(message);
+        return message;
     }
 }
