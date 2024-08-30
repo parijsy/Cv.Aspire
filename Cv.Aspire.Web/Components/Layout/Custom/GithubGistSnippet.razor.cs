73a34365ceb7f43fbc5f539f115d4f9587a5398b
Yuri Parijs <yuriparijs@hotmail.com>
2024-08-30 23:58:05 +0200

    Rewrote the GithubGistSnippet because the iframe didn't allowing linking to the gist. The new solution is a little fragile, but works in the current situation.

--- Cv.Aspire.Web/Components/Layout/Custom/GithubGistSnippet.razor.cs
+++ Cv.Aspire.Web/Components/Layout/Custom/GithubGistSnippet.razor.cs
@@ -1,29 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
+using System.Text.RegularExpressions;
 
 namespace Cv.Aspire.Web.Components.Layout.Custom;
 
 // Based on https://stackoverflow.com/a/70749777
-public class GithubGistSnippetBase : ComponentBase, IAsyncDisposable
+public partial class GithubGistSnippetBase : ComponentBase
 {
-    [Inject] private IJSRuntime JSRuntime { get; set; } = default!;
+    [Inject] private HttpClient HttpClient { get; set; } = default!;
 
     [Parameter, EditorRequired] public string Title { get; set; } = default!;
     [Parameter, EditorRequired] public string UserId { get; set; } = default!;
     [Parameter, EditorRequired] public string FileName { get; set; } = default!;
 
-    private IJSObjectReference? module;
-    protected string Id = Guid.NewGuid().ToString();
+    protected string stylesheet = string.Empty;
+    protected string snippet = string.Empty;
 
     protected override async Task OnInitializedAsync()
     {
-        module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./js/githubgist.js");
-        await module.InvokeVoidAsync("printSnippetFrom", Id, UserId, FileName);
+        var result = await HttpClient.GetAsync($"https://gist.github.com/{UserId}/{FileName}");
+        var text = await result.Content.ReadAsStringAsync();
+
+        // The html is written inbetween a document.write(''), so we pick up the content and write it directly.
+        // The first document.write will contain the stylesheet, and the second the snippet
+        var regex = DocumentWriteContentRegex();
+
+        var matches = regex.Matches(text);
+        stylesheet = matches[0].Groups[0].Value;
+        snippet = Regex.Unescape(matches[1].Groups[0].Value);
     }
 
-    public async ValueTask DisposeAsync()
-    {
-        if (module is not null)
-            await module.DisposeAsync();
-    }
+    [GeneratedRegex("(?<=document.write\\(')(.*)(?='\\))")]
+    internal static partial Regex DocumentWriteContentRegex();
 }
